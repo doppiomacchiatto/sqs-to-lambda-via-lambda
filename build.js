873047f133f4474351fe0f077275b5153f4faced
Zach Wily <zach@zwily.com>
2016-03-11 08:48:15 -0700

    switch to custom yaml schema for js minification

--- build.js
+++ build.js
@@ -1,20 +1,17 @@
 var yaml = require('js-yaml');
 var UglifyJS = require("uglify-js");
 var fs = require('fs');
-var traverse = require('traverse');
 
-function minify(path) {
-  return UglifyJS.minify(path).code;
-}
-
-var templateBody = fs.readFileSync('./cloudformation.yml', 'utf-8');
-var template = yaml.safeLoad(templateBody);
-
-traverse(template).forEach(function(val) {
-  if (typeof val === 'string' && val.indexOf('!js ') === 0) {
-    var code = val.slice(4);
-    this.update(eval(code));
+var MinifyYamlType = new yaml.Type('!minify', {
+  kind: 'scalar',
+  construct: function(data) {
+    return UglifyJS.minify(data).code;
   }
 });
+
+var SCHEMA = yaml.Schema.create([ MinifyYamlType ]);
+
+var templateBody = fs.readFileSync('./cloudformation.yml', 'utf-8');
+var template = yaml.load(templateBody, { schema: SCHEMA });
 
 fs.writeFileSync('./cloudformation.json', JSON.stringify(template, null, '  '), 'utf-8');
